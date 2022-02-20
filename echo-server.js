c612557d4c58bfd2df376a7bb147df9912e6f80d
Xander Uiterlinden <xander.uiterlinden@gmail.com>
2022-02-20 12:27:31 +0100

    Modified echo output

--- echo-server.js
+++ echo-server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-    response.write(request.url);
+    response.write("Requested: " + request.url + "\n");
     response.end();
     return;
 }).listen(8080);
